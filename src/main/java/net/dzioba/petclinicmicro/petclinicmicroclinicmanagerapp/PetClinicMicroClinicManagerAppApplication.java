24b07ba3b518ed4ecb9c4ec3ca7d8a7459b59bf4
piotrek19 <dziobap@gmail.com>
2019-12-25 13:55:13 +0100

    initial commit

---
+++ src/main/java/net/dzioba/petclinicmicro/petclinicmicroclinicmanagerapp/PetClinicMicroClinicManagerAppApplication.java
@@ -0,0 +1,13 @@
+package net.dzioba.petclinicmicro.petclinicmicroclinicmanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicMicroClinicManagerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicMicroClinicManagerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/dzioba/petclinicmicro/petclinicmicroclinicmanagerapp/PetClinicMicroClinicManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dzioba.petclinicmicro.petclinicmicroclinicmanagerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicMicroClinicManagerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
